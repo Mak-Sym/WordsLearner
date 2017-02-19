48443328a86d0b83e63f7d93e0263fa8aa348cfc
Maksym Fedoryshyn <mfedoryshyn@atlassian.com>
2017-02-20 00:08:22 +1100

    finished minimum functionality: play button and words change

--- app/src/main/java/com/maksym/android/words/learner/MainActivity.java
+++ app/src/main/java/com/maksym/android/words/learner/MainActivity.java
@@ -1,25 +1,91 @@
 package com.maksym.android.words.learner;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private TextToSpeech textToSpeech;
+    private final Random rnd = new Random(System.currentTimeMillis());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final ImageButton playButton = (ImageButton) findViewById(R.id.playwordbutton);
+        playButton.setEnabled(false);
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                TextView wordTextView = (TextView) findViewById(R.id.wordTextView);
+                String word = wordTextView.getText().toString();
+                play(word);
             }
         });
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.UK);
+                    playButton.setEnabled(true);
+                } else {
+                    Snackbar.make(findViewById(R.id.mainLayout), "Error initializing text to speech library.",
+                            Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                    fallbackToMediaPlayer();
+                }
+            }
+        });
+
+        renderNextWord();
     }
+
+    private void renderNextWord() {
+        TextView wordTextView = (TextView) findViewById(R.id.wordTextView);
+        wordTextView.setText(words[rnd.nextInt(words.length)]);
+    }
+
+    private void play(String textToPlay) {
+        if(!textToSpeech.isSpeaking()) {
+            textToSpeech.speak(textToPlay, TextToSpeech.QUEUE_FLUSH, null);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignored) { }
+            renderNextWord();
+        }
+    }
+
+    private void fallbackToMediaPlayer() {
+        // does nothing at the moment
+    }
+
+    @Override
+    public void onDestroy() {
+        if(textToSpeech.isSpeaking()) {
+            textToSpeech.stop();
+        }
+        textToSpeech.shutdown();
+        super.onDestroy();
+    }
+
+    private static final String words[] = { "a",
+                                            "and",
+                                            "be",
+                                            "I",
+                                            "in",
+                                            "is",
+                                            "it",
+                                            "of",
+                                            "that",
+                                            "the",
+                                            "to",
+                                            "was"};
 }
---
+++ app/src/main/java/com/maksym/android/words/learner/history/History.java
@@ -0,0 +1,13 @@
+package com.maksym.android.words.learner.history;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class History {
+    private final static int CAPACITY = 500;
+
+    private List<Integer> words = new ArrayList<>();
+    private int head = 0;
+
+
+}
